e95707df927b1cc941ed5ef0f47aed7164a629d6
Norival <xavier.laviron@gmx.fr>
2015-05-12 23:54:37 +0200

    tab2latex extension
    Add .tex extension to filename

--- tab2latex.R
+++ tab2latex.R
@@ -6,11 +6,11 @@
 
 
 tab2latex <- function(
-    X,                      #dataframe to convert
-    fileName = "tablatex",  #String: file name
-    showColumns = TRUE,     #Bool: show column names ?
-    showRows = FALSE,       #Bool: show row names ?
-    naString = "NA"         #String to insert instead of NA's
+    X,                          #dataframe to convert
+    fileName = "tablatex.tex",  #String: file name
+    showColumns = TRUE,         #Bool: show column names ?
+    showRows = FALSE,           #Bool: show row names ?
+    naString = "NA"             #String to insert instead of NA's
     )
 {
     write.table(
