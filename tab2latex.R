b627acffbfb7b6605ef5bb20309b108e0ac70dd3
Xavier Laviron <xavier.laviron@gmx.fr>
2015-05-14 19:11:50 +0200

    Add environment writing
    Add the possibility of adding environment table and environment tabular

--- tab2latex.R
+++ tab2latex.R
@@ -11,20 +11,52 @@ tab2latex <- function(
     showColumns = TRUE,         #Bool: show column names ?
     showRows = FALSE,           #Bool: show row names ?
     naString = "NA",            #String to insert instead of NA's
+    align = "c",                #Alignement: "c", "r", "l" [c]
+    caption = "",               #Caption. None if not defined
+    label = "",                 #Label. None if not defined
+    float = FALSE,              #Bool: make a table environment ?
     ...                         #Take X if 'X=' is missing
     )
 {
     if (missing(X))
         X = ...
+        
+    if (showRows == TRUE)
+        nColumns = length(X)+1
+    else
+        nColumns = length(X)
     
+    con <- file(description = fileName, open = "w")
+
+    if (float == TRUE)
+        cat("\\begin{table}\n", file = con)
+    if (caption != "")
+        cat("\\caption{", caption, "}\n", sep = "", file = con)
+    if (label != "")
+        cat("\\label{", label, "}\n", sep = "", file = con)
+
+    cat("\\begin{tabular}{", rep(x = align, nColumns), "}\n", sep = "", file = con)
+
+    if (showRows == TRUE & showColumns == TRUE)
+        cat(" ", colnames(X), sep = " & ", file = con)
+    else if (showColumns == TRUE)
+        cat(colnames(X), sep = " & ", file = con)
+    cat(" \\\\ \n", file = con)
+
     write.table(
         x = X,
-        file = fileName,
+    #    file = fileName,
+        file = con,
         na = naString,
-        col.names = showColumns,
+        col.names = FALSE,
         row.names = showRows,
         sep = " & ",
         quote = FALSE,
         eol = " \\\\\n"
     )
+    cat("\\end{tabular}\n", file = con)
+    if (float == TRUE)
+        cat("\\end{table}\n", file = con)
+
+    close(con)
 }
