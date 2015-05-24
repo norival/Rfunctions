46354f7e7ac4f93f6070278b77154552c0ffebd9
Norival <xavier.laviron@gmx.fr>
2015-05-24 20:22:11 +0200

    Avancement tabInput

--- tabInput.R
+++ tabInput.R
@@ -4,9 +4,22 @@ tabInput  <- function(
 
 {
   print("Nombre de colonnes ?")
-    nColumns <- scan()
+    nColumns <- scan(nmax = 1)
   print("Nombre de lignes ?")
-    nRows <- scan()
+    nRows <- scan(nmax = 1)
   print("Nom des colonnes ?")
     colNames <- scan(what = 'character', nmax = nColumns)
   
+  tabTmp <- matrix(nrow = nRows, ncol = nColumns)
+
+  i <- 1
+  while (i <= nColumns)
+  {
+    print(paste(colNames[i], "?"))
+    j <- 1
+    while (j <= nRows)
+    {
+      print(paste("ligne", j))
+      tabTmp[j, i] <- scan(nmax = 1, quiet = TRUE)
+      j = j+1
+    }
