78475016829647c391aeca6739f4d6c0aef62955
Norival <xavier.laviron@gmx.fr>
2015-05-25 11:28:15 +0200

    Fonction tabInput

--- tabInput.R
+++ tabInput.R
@@ -7,18 +7,29 @@ tabInput  <- function(
   print("Nom des colonnes ?")
     colNames <- scan(what = 'character', nmax = ncols, quiet = TRUE)
   
-  tabTmp <- matrix(nrow = nrows, ncol = nColumns)
+  tabTmp <- data.frame(c(rep('A', nrows)))
 
   i <- 1
   while (i <= ncols)
   {
     print(paste(colNames[i], "?"))
-    a <- scan(nmax = nrows, quiet = TRUE, what = c('character', 'numeric'))
-    tabTmp[, i] <- a
+    print("Numeric or charcater ?(n/c)")
+    rep <- scan(n = 1, what = 'character')
+    if (rep == 'c')
+    {
+      a <- scan(nmax = nrows, quiet = TRUE, what = 'character')
+      tabTmp <- cbind.data.frame(tabTmp, a)
+    }
+    else
+    {
+      a <- scan(n = nrows, quiet = TRUE, blank.lines.skip = TRUE)
+      tabTmp <- cbind.data.frame(tabTmp, as.numeric(a))
+    }
     i = i+1
   }
-  b <- as.data.frame(tabTmp)
-  colnames(b) <- colNames
+  
+b <- tabTmp[, 2:length(tabTmp)]
+colnames(b) <- colNames
 
-  return(b)
+return(b)
 }
