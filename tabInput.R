07b96b4f8d0c96b934314f9f82fdee37785052e4
tonomorf <fromonotcamille@gmail.com>
2015-05-27 19:44:48 +0200

    PROUT
    Pouet

--- tabInput.R
+++ tabInput.R
@@ -12,7 +12,7 @@
 tabInput  <- function(
                       nrows,
                       ncols
-                      )
+                      )                      CACA
 
 {
   print("Columns' names?")
